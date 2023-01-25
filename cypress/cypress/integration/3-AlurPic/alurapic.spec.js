22a2f1302d477070366374c112222a0250d19ed8
rldcarvalho <rldcarvalhoo@gmail.com>
2023-01-25 20:14:32 -0300

    add valid registration information test

--- cypress/cypress/integration/3-AlurPic/alurapic.spec.js
+++ cypress/cypress/integration/3-AlurPic/alurapic.spec.js
@@ -12,4 +12,25 @@ describe("Login e registro de usuarios alura pic", () => {
     cy.contains("ap-vmessage", "Full name is required!").should("be.visible");
     cy.contains("ap-vmessage", "User name is required!").should("be.visible");
   });
+
+  it("verifica mensagens de email invalido", () => {
+    cy.contains("a", "Register now").click();
+    cy.get('input[formcontrolname="email"]').type("rafael");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Invalid e-mail").should("be.visible");
+  });
+
+  it("verifica mensagens de senha com menos de oito caracteres", () => {
+    cy.contains("a", "Register now").click();
+    cy.get('input[formcontrolname="password"]').type("123");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Mininum length is 8").should("be.visible");
+  });
+
+  it("verifica mensagens de username com letra maiuscula", () => {
+    cy.contains("a", "Register now").click();
+    cy.get('input[formcontrolname="userName"]').type("Rafael");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Must be lower case").should("be.visible");
+  });
 });
