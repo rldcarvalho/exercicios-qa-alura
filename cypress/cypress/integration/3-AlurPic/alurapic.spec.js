3bbea2d296d19943c3cb374838e2f61d8a6ffb5e
rldcarvalho <rldcarvalhoo@gmail.com>
2023-01-27 16:29:36 -0300

    add fixture exemple

--- cypress/cypress/integration/3-AlurPic/alurapic.spec.js
+++ cypress/cypress/integration/3-AlurPic/alurapic.spec.js
@@ -1,3 +1,5 @@
+const { it } = require("mocha");
+
 describe("Login e registro de usuarios alura pic", () => {
   beforeEach(() => {
     cy.visit("https://alura-fotos.herokuapp.com");
@@ -44,5 +46,18 @@ describe("Login e registro de usuarios alura pic", () => {
   it("fazer login com usuario valido", () => {
     cy.login("flavio", "123");
     cy.contains("a", "(Logout)").should("be.visible");
+  });
+
+  const usuarios = require("../../fixtures/usuarios.json");
+  usuarios.forEach((usuario) => {
+    it.only(`registra novo usuário ${usuario.userName}`, () => {
+      cy.contains("a", "Register now").click();
+      cy.contains("button", "Register").click();
+      cy.get('input[formcontrolname="email"]').type(usuario.email);
+      cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+      cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+      cy.get('input[formcontrolname="password"]').type(usuario.password);
+      cy.contains("button", "Register").click();
+    });
   });
 });
